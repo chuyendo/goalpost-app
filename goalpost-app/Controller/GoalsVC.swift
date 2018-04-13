efa6156333e96017c332f14cc3460ffa7d29f200
Cherry <hanhchuyen6791@yahoo.com>
2018-04-13 23:16:06 +0700

    Building GoalsVC

---
+++ goalpost-app/Controller/GoalsVC.swift
@@ -0,0 +1,25 @@
+//
+//  GoalsVC.swift
+//  goalpost-app
+//
+//  Created by mac on 4/12/18.
+//  Copyright © 2018 DoThuyHanhChuyen. All rights reserved.
+//
+
+import UIKit
+
+class GoalsVC: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    @IBAction func addGoalBtnWasPressed(_ sender: Any) {
+        print("Button was pressed")
+    }
+    
+}
+
