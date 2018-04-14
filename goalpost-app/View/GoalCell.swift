8373625602583b469f27ea18f17e35d936b19332
Cherry <hanhchuyen6791@yahoo.com>
2018-04-14 16:46:34 +0700

    Creating Goal Core Data Entity & Attributes

--- goalpost-app/View/GoalCell.swift
+++ goalpost-app/View/GoalCell.swift
@@ -14,9 +14,9 @@ class GoalCell: UITableViewCell {
     @IBOutlet weak var goalTypeLbl: UILabel!
     @IBOutlet weak var goalProgressLbl: UILabel!
     
-    func configueCell(description: String, type: String, goalProgressAmount: Int) {
+    func configueCell(description: String, type: GoalType, goalProgressAmount: Int) {
         self.goalProgressLbl.text = description
-        self.goalTypeLbl.text = type
+        self.goalTypeLbl.text = type.rawValue
         self.goalProgressLbl.text = String(describing: goalProgressAmount)
     }
 
