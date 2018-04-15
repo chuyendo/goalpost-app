aa7b580e29023e7550881d65ba663fe899321ee3
Cherry <hanhchuyen6791@yahoo.com>
2018-04-15 13:14:27 +0700

    Creating a UIViewController Extension

--- goalpost-app/Controller/GoalsVC.swift
+++ goalpost-app/Controller/GoalsVC.swift
@@ -19,7 +19,8 @@ class GoalsVC: UIViewController {
     }
     
     @IBAction func addGoalBtnWasPressed(_ sender: Any) {
-        print("Button was pressed")
+     guard let createGoalVC = storyboard?.instantiateViewController(withIdentifier: "CreateGoalVC") else { return }
+     presentDetail(viewControllerToPresent: createGoalVC)
     }
 
 }
